34f4c95da487e14ced925953b49d0531d794610e
Bittu <bittu1040@gmail.com>
2023-09-30 17:43:55 +0530

    console error

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +7,6 @@ import { Subject } from 'rxjs';
 export class SharedService {
 
   logoutButtonFlag= new Subject<boolean>();
-  loginButtonFlag= new Subject<boolean>();
+  loginButtonFlag= new  BehaviorSubject<boolean>(true);
   constructor() { }
 }
