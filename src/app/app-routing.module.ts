84ac0627fdc8e3b71716fb52edbcb4d091ff8c6c
Bittu <bittu1040@gmail.com>
2023-10-08 18:02:46 +0530

    Blog component content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { authGaurdGuard } from './gaurds/auth-gaurd.guard';
 import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  {path: 'home', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate:[authGaurdGuard] },
+  { path: 'blog', component: BlogComponent},
+  { path: '**', component:PageNotFoundComponent},
 ];
 
 @NgModule({
