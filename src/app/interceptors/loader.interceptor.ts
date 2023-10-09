e8fc77bfb01f003b9c64772f09502445d584242b
Bittu <bittu1040@gmail.com>
2023-10-09 22:33:10 +0530

    Added loader visible time for minimum 800ms

--- src/app/interceptors/loader.interceptor.ts
+++ src/app/interceptors/loader.interceptor.ts
@@ -5,7 +5,7 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable, delay, finalize } from 'rxjs';
+import { Observable, delay, finalize, switchMap, timer } from 'rxjs';
 import { LoaderService } from '../services/loader.service';
 
 @Injectable()
@@ -15,11 +15,22 @@ export class LoaderInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.loaderService.isVisible.next(true);
-    return next.handle(request).pipe(
+    return timer(800).pipe(
+      switchMap(() => next.handle(request)),
       finalize(() => {
         this.loaderService.isVisible.next(false);
       })
     );
   }
+
+
+  // intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  //   this.loaderService.isVisible.next(true);
+  //   return next.handle(request).pipe(
+  //     finalize(() => {
+  //       this.loaderService.isVisible.next(false);
+  //     })
+  //   );
+  // }
 
 }
