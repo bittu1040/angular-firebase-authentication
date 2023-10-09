15c4c01df962a769f474f88791a771391f65d855
Bittu <bittu1040@gmail.com>
2023-10-10 00:34:41 +0530

    blog component

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss']
 })
-export class BlogComponent {
+export class BlogComponent implements OnInit {
+  selectedCategory: string = 'All'; 
 
+  boxes = [
+    { category: 'js', visible: false },
+    { category: 'js', visible: false },
+    { category: 'js', visible: false },
+    { category: 'js', visible: false },
+    { category: 'angular', visible: false },
+    { category: 'angular', visible: false },
+    { category: 'angular', visible: false },
+    { category: 'angular', visible: false },
+    { category: 'css', visible: false },
+    { category: 'css', visible: false },
+    { category: 'css', visible: false },
+    { category: 'css', visible: false },
+  ];
 
-  progressValue: number = 0; // Default value of 0 for the progress bar
+  ngOnInit(): void {
+    this.showAll()
+  }
 
-  updateProgressBar(value: number): void {
-    this.progressValue = value;
+  showAll() {
+    this.selectedCategory = 'All';
+    this.boxes.forEach(box => (box.visible = true));
+  }
+
+  filterCategory(category: string) {
+    this.selectedCategory = category;
+    this.boxes.forEach(box => {
+      box.visible = box.category === category;
+    });
   }
 }
